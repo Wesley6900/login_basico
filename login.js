9db22398b408ba8e24b1bce3c9aec90b983890ca
Wesley sp <127230624+Wesley6900@users.noreply.github.com>
2023-07-18 16:39:16 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
-let usuario_cadastrado = []
+var usuario_cadastrado = [];
 
-function Cadastrar() {
-    var login_text_cadastre = window.document.getElementById('login_text_cadastrar').value;
-    var email_cadastrar = window.document.getElementById('email_cadastrar').value;
-    var senha_number_cadastrar = window.document.getElementById('senha_number_cadastrar').value;
+
+function cadastrar(){
+    var login_text_cadastre = document.getElementById('login_text_cadastrar').value;
+    var email_cadastrar = document.getElementById('email_cadastrar').value;
+    var senha_number_cadastrar = document.getElementById('senha_number_cadastrar').value;
 
 
     let cadastro_usuarios = {
@@ -12,8 +13,31 @@ function Cadastrar() {
         'senha' : senha_number_cadastrar,
     }
 
-    usuario_cadastrado.push(cadastro_usuarios)
+
+    if (cadastro_usuarios.usuario != '' && email_cadastrar != '' && senha_number_cadastrar != '') {
+        usuario_cadastrado.push(cadastro_usuarios)
+        alert('cadastro bem sucedido!!!')
+
+        document.getElementById('login_text_cadastrar').value = '';
+        document.getElementById('email_cadastrar').value = '';
+        document.getElementById('senha_number_cadastrar').value = '';
+    }
 
     console.log(usuario_cadastrado);
+
+    alert('Usuario cadastrado!!!');
+};
+
+
+function login() {
+    var login_acessar = window.document.getElementById('login_text').value;
+    var senha_acessar = window.document.getElementById('senha_number').value;
+
+    let usuarioEncontrado = usuario_cadastrado.find(cadastro => cadastro.usuario === login_acessar && cadastro.senha === senha_acessar);
+    if (usuarioEncontrado) {
+        alert('Seja bem vindo ' + login_acessar);
+    }else{
+        alert('senha invalida!!!!')
+    }
 }
 
